cd021d3b1f80571d45d31b1cb8325398b90026aa
guillermo <guillermolm95@gmail.com>
2023-02-02 21:57:03 +0100

    Clase Guille

---
+++ src/Guille/Guille.java
@@ -0,0 +1,13 @@
+package Guille;
+
+public class Guille {
+	
+	public static void main(String[] args) {
+		presentacion();
+	}
+
+	public static void presentacion() {
+		// TODO Auto-generated method stub
+		System.out.println("Hola soy Guille");
+	}
+}
