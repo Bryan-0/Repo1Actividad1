d6fef79796511bbcee7c0d6118013173375cfe85
Kevin Ayala <005.kevin.ayala@gmail.com>
2023-02-02 21:53:34 +0100

    Agregar clase Kevin

---
+++ src/Kevin/Kevin.java
@@ -0,0 +1,16 @@
+package Kevin;
+
+public class Kevin {
+	
+	public static void saludo() {
+		System.out.println("Hola, soy Kevin");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		saludo();
+		
+	}
+	
+}
