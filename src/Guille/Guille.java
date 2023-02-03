3726a78ca4eb1115d380a1bfb7832cac6fb3a0bf
guillermo <guillermolm95@gmail.com>
2023-02-03 21:18:07 +0100

    hobbies

--- src/Guille/Guille.java
+++ src/Guille/Guille.java
@@ -4,10 +4,14 @@ public class Guille {
 	
 	public static void main(String[] args) {
 		presentacion();
+		hobbies();
 	}
 
 	public static void presentacion() {
 		// TODO Auto-generated method stub
 		System.out.println("Hola soy Guille");
+	}
+	public static void hobbies() {
+		System.out.println("Me gusta el baloncesto, el cine y la escalada");
 	}
 }
