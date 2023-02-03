0ca06bcb49713807e2c221c0e89c28e9325d71ae
Kevin Ayala <005.kevin.ayala@gmail.com>
2023-02-03 21:18:07 +0100

    Agregar hobbies a clase Kevin

--- src/Kevin/Kevin.java
+++ src/Kevin/Kevin.java
@@ -6,6 +6,10 @@ public class Kevin {
 		System.out.println("Hola, soy Kevin");
 	}
 	
+	public static void hobbies() {
+		System.out.println("Me gusta Leer, los videojuegos y la musica.");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		
